bbd0499797d93dd5e4dbe7a419876c95482f30eb
zelson71 <zelson1@gmail.com>
2020-02-27 10:02:50 -0700

    need to get Projects formatted

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,84 +1,154 @@
-const Portfolio = function() {
-	function makeWords() {
-		var words = [
-			{
-				text: "Analysing",
-				weight: 12.3
-			}, {
-				text: "css3",
-				weight: 8
-			}, {
-				text: "javascript",
-				weight: 14
-			}, {
-				text: "jquery",
-				weight: 3
-			}, {
-				text: "programming",
-				weight: 7
-			}, {
-				text: "python",
-				weight: 10
-			}, {
-				text: "java",
-				weight: 9
-			}, {
-				text: "Flask",
-				weight: 15
-			}, {
-				text: "JQuery",
-				weight: 7
-			}
-		];
-		return words;
-	}
+/*  ======================= SETUP ======================= */
+var config = {
+    trace: true,
+    spiralResolution: 1, //Lower = better resolution
+    spiralLimit: 360 * 5,
+    lineHeight: 0.8,
+    xWordPadding: 0,
+    yWordPadding: 3,
+    font: "sans-serif"
+}
 
-	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
-	}
+var words = ["Analysing", "css3", "javascript", "programming", "python", "C#", "Flask", "SQL", "Machine Learning", "Plotly", "Seaborn"].map(function (word) {
+    return {
+        word: word,
+        freq: Math.floor(Math.random() * 50) + 10
+    }
+})
 
-	function displayWordCloud() {
-		var count = 1;
-		$(window).on('scroll', function() {
-			var y_scroll_pos = window.pageYOffset;
-			var scroll_pos_test = 2700; // set to whatever you want it to be
-			var words = makeWords();
-			if (y_scroll_pos > scroll_pos_test && count <= 1) {
-				makeWordCloud(words);
-				count++;
-			}
-		});
-	}
+words.sort(function (a, b) {
+    return -1 * (a.freq - b.freq);
+});
 
-	function designForm() {
-		$("#my-modal form").addClass("my-form");
-	}
+var cloud = document.getElementById("word-cloud");
+cloud.style.position = "relative";
+cloud.style.fontFamily = config.font;
 
-	function typeAnimation() {
-		Typed.new("#writing-text", {
-			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "Am interested in Machine Learning.", "solve problems."
-			],
-			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-			stringsElement: null,
-			// typing speed
-			typeSpeed: 1,
-			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
-		});
-	}
+var traceCanvas = document.createElement("canvas");
+traceCanvas.width = cloud.offsetWidth;
+traceCanvas.height = cloud.offsetHeight;
+var traceCanvasCtx = traceCanvas.getContext("2d");
+cloud.appendChild(traceCanvas);
 
-	return {
-		displayWordCloud: displayWordCloud,
-		typeAnimation: typeAnimation
-	}
+var startPoint = {
+    x: cloud.offsetWidth / 2,
+    y: cloud.offsetHeight / 2
+};
 
-}();
+var wordsDown = [];
+/* ======================= END SETUP ======================= */
 
 
-Portfolio.displayWordCloud();
-Portfolio.typeAnimation();
+
+
+
+/* =======================  PLACEMENT FUNCTIONS =======================  */
+function createWordObject(word, freq) {
+    var wordContainer = document.createElement("div");
+    wordContainer.style.position = "absolute";
+    wordContainer.style.fontSize = freq + "px";
+    wordContainer.style.lineHeight = config.lineHeight;
+    /*    wordContainer.style.transform = "translateX(-50%) translateY(-50%)";*/
+    wordContainer.appendChild(document.createTextNode(word));
+
+    return wordContainer;
+}
+
+function placeWord(word, x, y) {
+
+    cloud.appendChild(word);
+    word.style.left = x - word.offsetWidth / 2 + "px";
+    word.style.top = y - word.offsetHeight / 2 + "px";
+
+    wordsDown.push(word.getBoundingClientRect());
+}
+
+function trace(x, y) {
+    //     traceCanvasCtx.lineTo(x, y);
+    //     traceCanvasCtx.stroke();
+    traceCanvasCtx.fillRect(x, y, 1, 1);
+}
+
+function spiral(i, callback) {
+    angle = config.spiralResolution * i;
+    x = (1 + angle) * Math.cos(angle);
+    y = (1 + angle) * Math.sin(angle);
+    return callback ? callback() : null;
+}
+
+function intersect(word, x, y) {
+    cloud.appendChild(word);
+
+    word.style.left = x - word.offsetWidth / 2 + "px";
+    word.style.top = y - word.offsetHeight / 2 + "px";
+
+    var currentWord = word.getBoundingClientRect();
+
+    cloud.removeChild(word);
+
+    for (var i = 0; i < wordsDown.length; i += 1) {
+        var comparisonWord = wordsDown[i];
+
+        if (!(currentWord.right + config.xWordPadding < comparisonWord.left - config.xWordPadding ||
+            currentWord.left - config.xWordPadding > comparisonWord.right + config.wXordPadding ||
+            currentWord.bottom + config.yWordPadding < comparisonWord.top - config.yWordPadding ||
+            currentWord.top - config.yWordPadding > comparisonWord.bottom + config.yWordPadding)) {
+
+            return true;
+        }
+    }
+
+    return false;
+}
+/* =======================  END PLACEMENT FUNCTIONS =======================  */
+
+
+
+
+
+/* =======================  LETS GO! =======================  */
+(function placeWords() {
+    for (var i = 0; i < words.length; i += 1) {
+
+        var word = createWordObject(words[i].word, words[i].freq);
+
+        for (var j = 0; j < config.spiralLimit; j++) {
+            //If the spiral function returns true, we've placed the word down and can break from the j loop
+            if (spiral(j, function () {
+                if (!intersect(word, startPoint.x + x, startPoint.y + y)) {
+                    placeWord(word, startPoint.x + x, startPoint.y + y);
+                    return true;
+                }
+            })) {
+                break;
+            }
+        }
+    }
+})();
+/* ======================= WHEW. THAT WAS FUN. We should do that again sometime ... ======================= */
+
+
+
+/* =======================  Draw the placement spiral if trace lines is on ======================= */
+(function traceSpiral() {
+
+    traceCanvasCtx.beginPath();
+
+    if (config.trace) {
+        var frame = 1;
+
+        function animate() {
+            spiral(frame, function () {
+                trace(startPoint.x + x, startPoint.y + y);
+            });
+
+            frame += 1;
+
+            if (frame < config.spiralLimit) {
+                window.requestAnimationFrame(animate);
+            }
+        }
+
+        animate();
+    }
+})();
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,64 +1,119 @@
-const TypeWriter = function (txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
-
-// Typing Method
-TypeWriter.prototype.type = function () {
-    //current index of word
-    const current = this.words % this.words.length;
-    //Get the full Text of current word
-    const fullTxt = this.words[current];
-
-    //check if word is deleting
-    if (this.isDeleting) {
-        //Remove a charachter
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-
-
-
-    } else {
-        //add a charachter
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-    //insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-    //Type Speed
-    let typeSpeed = 300;
-    if (this.isDeleting) {
-        typeSpeed /= 2;
-    }
-
-    //if word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
-        //make a pause at the end
-        typeSpeed = this.wait;
-        //set delete to True
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
         this.isDeleting = false;
-        this.wordIndex++;
-        //pause before typing next word
-        typeSpeed = 800;
     }
-    setTimeout(() => this.type(), 500)
+
+    type() {
+        // Current index of word
+        const current = this.wordIndex % this.words.length;
+        // Get full text of current word
+        const fullTxt = this.words[current];
+
+        // Check if deleting
+        if (this.isDeleting) {
+            // Remove char
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            // Add char
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        // Insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+        // Initial Type Speed
+        let typeSpeed = 300;
+
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        // If word is complete
+        if (!this.isDeleting && this.txt === fullTxt) {
+            // Make pause at end
+            typeSpeed = this.wait;
+            // Set delete to true
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            // Move to next word
+            this.wordIndex++;
+            // Pause before start typing
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
 }
-// init on DOM Load
+
+
+// Init On DOM Load
 document.addEventListener('DOMContentLoaded', init);
 
-//Init App
-
+// Init App
 function init() {
     const txtElement = document.querySelector('.txt-type');
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
-    // Initialize the typewriter
+    // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
+}
 
+const Portfolio = function () {
+    function makeWords() {
+        var words = [
+            {
+                text: "Analysing",
+                weight: 12.3
+            }, {
+                text: "css3",
+                weight: 8
+            }, {
+                text: "javascript",
+                weight: 14
+            }, {
+                text: "jquery",
+                weight: 3
+            }, {
+                text: "programming",
+                weight: 7
+            }, {
+                text: "python",
+                weight: 10
+            }, {
+                text: "java",
+                weight: 9
+            }, {
+                text: "Flask",
+                weight: 15
+            }, {
+                text: "JQuery",
+                weight: 7
+            }
+        ];
+        return words;
+    }
+
+    function makeWordCloud(words) {
+        $('.teaching-domains').jQCloud(words, { delay: 120 });
+    }
+
+    function displayWordCloud() {
+        var count = 1;
+        $(window).on('scroll', function () {
+            var y_scroll_pos = window.pageYOffset;
+            var scroll_pos_test = 2700; // set to whatever you want it to be
+            var words = makeWords();
+            if (y_scroll_pos > scroll_pos_test && count <= 1) {
+                makeWordCloud(words);
+                count++;
+            }
+        });
+    }
 }
